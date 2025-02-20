84a1570603b0c776fc08a5e320a430f23eddde4e
= <=>
2025-02-20 14:31:21 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-',app()->getLocale()) }}">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PWL 2024</title>
+</head>
+<body>
+<h2>Pemrograman Web Lanjut 2024</h2>
+<h3>Semangat Belajar Laravel 10</h3>
+</body>
+</html>
